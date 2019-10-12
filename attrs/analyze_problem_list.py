23703a6fb5028d982b3febc630e28f9bb65a82a6
guofei9987 <guofei9987@foxmail.com>
2019-10-12 21:29:03 +0800

    AutoUpload

--- attrs/analyze_problem_list.py
+++ attrs/analyze_problem_list.py
@@ -1,19 +1,25 @@
 # analyze every *.py and list the status
 import os
-file_list=os.listdir('..')
-solved_list=[]
-unsolved_list=[]
+import shutil
+
+file_list = os.listdir('..')
+solved_list = []
+unsolved_list = []
 for i in file_list:
-    if i[0]=='[':
-        file_name='../'+i
+    if i[0] == '[':
+        file_name = '../' + i
         print(file_name)
-        f=open(file_name,'r',encoding='utf-8')
-        if len(f.readlines())>4:
+        f = open(file_name, 'r', encoding='utf-8')
+        if len(f.readlines()) > 4:
+            f.close()
             solved_list.append(i)
+            shutil.move('../'+i, '../solved/' + i)
         else:
             unsolved_list.append(i)
-        f.close()
+            f.close()
 
-print('number of unsolved: ',len(unsolved_list))
-print('number of solved: ',len(solved_list))
+
+print('number of unsolved: ', len(unsolved_list))
+print('number of solved: ', len(solved_list))
+
 
